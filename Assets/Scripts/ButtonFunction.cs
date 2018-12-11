d3c74f767ab1e65879ba8eab6026b94adec34b72
TheZeus93 <thezeus93@gmail.com>
2018-12-11 11:47:03 +0100

    Progetto per testare le singole funzioni della comunicazione con il db

---
+++ Assets/Scripts/ButtonFunction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFunction : MonoBehaviour {
+
+    public Function function;
+
+    public enum Function {
+        function1,
+        function2,
+        function3,
+        function4
+    }
+
+    private void Start() {
+
+        Button mButton = GetComponent<Button>();
+
+        if (function == Function.function1)
+            mButton.onClick.AddListener(() => Function1());
+
+        if (function == Function.function2)
+            mButton.onClick.AddListener(() => Function2());
+
+        if (function == Function.function3)
+            mButton.onClick.AddListener(() => Function3());
+
+        if (function == Function.function4)
+            mButton.onClick.AddListener(() => Function4());
+
+    }
+
+    public void Function1() {
+        print("Funzione 1");
+    }
+
+    public void Function2() {
+        print("Funzione 2");
+    }
+
+    public void Function3() {
+        print("Funzione 3");
+    }
+
+    public void Function4() {
+        print("Funzione 4");
+    }
+
+}
